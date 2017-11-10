eb1f51ffbdeb8679f986103781175086b2759a58
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-11-10 07:51:51 -0800

    Tidy _bash_ parameter

--- tasks.py
+++ tasks.py
@@ -24,14 +24,6 @@ def task_generic_bash_script(self):
 
 def gen_task(script, inputs, outputs, parameters={}):
     parameters['_bash_'] = script
-    # Like snakemake, we use bash "strict mode", but we add -vx.
-    # http://redsymbol.net/articles/unofficial-bash-strict-mode/
-    prefix = """
-set -vxeuo pipefail
-IFS=$'\n\t'
-
-"""
-    script = prefix + script
     make_task = PypeTask(
             inputs={k: makePypeLocalFile(v) for k,v in inputs.iteritems()},
             outputs={k: makePypeLocalFile(v) for k,v in outputs.iteritems()},
